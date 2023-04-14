1a91824ecf80b0b599498d57b49e4a5e56d8f7dd
alokkusingh <alok.ku.singh@gmail.com>
2023-04-14 23:36:55 +0530

    Updated cron from config

--- src/main/java/com/alok/home/scheduler/HomeSchedulerServiceApplication.java
+++ src/main/java/com/alok/home/scheduler/HomeSchedulerServiceApplication.java
@@ -14,32 +14,33 @@ public class HomeSchedulerServiceApplication {
 
 	@Autowired
 	private RestTemplate restTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HomeSchedulerServiceApplication.class, args);
 	}
 
-	@Scheduled(cron = "0 0 */12 * * ?")
+	@Scheduled(cron = "${cron.expense}")
 	public void scheduleExpenseSheetRefresh() {
 		log.info("Cron - ExpenseSheetRefresh");
 		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/home/etl/gsheet/refresh/expense", String.class);
 		log.info("Response Code: {}, Response Message: {}", response.getStatusCode(), response.getBody());
 	}
 
-	@Scheduled(cron = "0 5 9 * * ?")
+	@Scheduled(cron = "${cron.tax}")
 	public void scheduleTaxSheetRefresh() {
 		log.info("Cron - TaxSheetRefresh");
 		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/home/etl/gsheet/refresh/tax", String.class);
 		log.info("Response Code: {}, Response Message: {}", response.getStatusCode(), response.getBody());
 	}
 
-	@Scheduled(cron = "0 10 9 * * ?")
+	@Scheduled(cron = "${cron.investment}")
 	public void scheduleInvestmentSheetRefresh() {
 		log.info("Cron - InvestmentSheetRefresh");
 		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/home/etl/gsheet/refresh/investment", String.class);
 		log.info("Response Code: {}, Response Message: {}", response.getStatusCode(), response.getBody());
 	}
 
-	@Scheduled(cron = "0 15 9 * * ?")
+	@Scheduled(cron = "${cron.odion}")
 	public void scheduleOdionSheetRefresh() {
 		log.info("Cron - OdionSheetRefresh");
 		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/home/etl/gsheet/refresh/odion/transactions", String.class);
